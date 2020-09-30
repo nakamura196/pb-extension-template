6602418702ea7ca7189d7481486f4baac3ba5e48
nakamura196 <na.kamura.1263@gmail.com>
2020-09-30 12:22:41 +0900

    add

--- src/pb-iframe.js
+++ src/pb-iframe.js
@@ -13,19 +13,34 @@ export class PbIframe extends pbMixin(LitElement) {
             height : {
                 type: String
             },
+            label : {
+                type: String
+            },
             ...super.properties
         };
     }
 
     constructor() {
         super();
+        this.label = "はじめてのラベル"
         this.url = 'https://teipublisher.com/index.html';
         this.width = '100%';
         this.height = '600';
     }
 
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.subscribeTo('pb-geolocation', (ev) => {
+            if (ev.detail.label) {
+                this.label = ev.detail.label;
+            }
+        });
+    }
+
     render() {
         return html`
+            ${this.label}
             <iframe width="${this.width}" height="${this.height}" allowfullscreen="true" frameborder="0" src="${this.url}"/>
         `;
     }
