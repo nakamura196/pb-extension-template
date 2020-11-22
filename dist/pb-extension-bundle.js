bc4dadd361f1d359cea9e3f4a67535280b6cfc71
nakamura196 <na.kamura.1263@gmail.com>
2020-11-22 17:20:52 +0900

    remove clipboard

--- dist/pb-extension-bundle.js
+++ dist/pb-extension-bundle.js
@@ -1,28 +1,6 @@
-import{t as e}from"./paper-inky-focus-behavior-b4765f0e.js";import{p as t,L as i,_ as r,h as o,c as n}from"./pb-mixin-ad783a5d.js";import"./paper-icon-button-ca3015ec.js";class s extends(t(i)){static get properties(){return r({label:{type:String}},super.properties)}constructor(){super(),this.label="clipboard.label"}render(){return o`
-            <h3>${e(this.label)}</h3>
-            <div>
-                <slot></slot>
-                <paper-icon-button icon="icons:content-copy" @click="${this._copy}"
-                    title="${e("clipboard.copy")}"></paper-icon-button>
-            </div>
-        `}_copy(){const e=this.shadowRoot.querySelector("slot"),t=document.createElement("div");e.assignedNodes().forEach(e=>{t.appendChild(document.importNode(e,!0))}),navigator.clipboard.writeText(t.innerText)}static get styles(){return n`
-            :host {
-                display: block;
-            }
-            h3 {
-                margin: 0;
-                font-size: .85em;
-                font-weight: normal;
-                color: #3a3a3a;
-            }
-            div {
-                display: flex;
-                align-items: center;
-                padding: 0 16px;
-            }
-        `}}customElements.define("pb-clipboard",s);class a extends(t(i)){static get properties(){return r({url:{type:String},width:{type:String},height:{type:String}},super.properties)}constructor(){super(),this.url="https://www.google.com/",this.width="100%",this.height="600"}connectedCallback(){super.connectedCallback(),this.subscribeTo("pb-keyword",e=>{e.detail.url&&(this.url=e.detail.url)})}render(){return o`
+import"./paper-inky-focus-behavior-b4765f0e.js";import{p as e,L as t,_ as r,h as i,c as n}from"./pb-mixin-ad783a5d.js";import"./paper-icon-button-ca3015ec.js";class o extends(e(t)){static get properties(){return r({url:{type:String},width:{type:String},height:{type:String}},super.properties)}constructor(){super(),this.url="https://www.google.com/",this.width="100%",this.height="600"}connectedCallback(){super.connectedCallback(),this.subscribeTo("pb-keyword",e=>{e.detail.url&&(this.url=e.detail.url)})}render(){return i`
             <iframe width="${this.width}" height="${this.height}" allowfullscreen="true" frameborder="0" src="${this.url}"/>
-        `}}customElements.define("pb-iframe",a);class l extends(t(i)){static get properties(){return r(r({},super.properties),{},{url:{type:String},label:{type:String}})}connectedCallback(){super.connectedCallback(),this.addEventListener("click",()=>this.emitTo("pb-keyword",{url:this.url,label:this.label}))}render(){return o`<span id="content"><slot></slot></span>`}static get styles(){return n`
+        `}}customElements.define("pb-iframe",o);class s extends(e(t)){static get properties(){return r(r({},super.properties),{},{url:{type:String},label:{type:String}})}connectedCallback(){super.connectedCallback(),this.addEventListener("click",()=>this.emitTo("pb-keyword",{url:this.url,label:this.label}))}render(){return i`<span id="content"><slot></slot></span>`}static get styles(){return n`
             :host {
                 display: inline;
             }
@@ -52,4 +30,4 @@ import{t as e}from"./paper-inky-focus-behavior-b4765f0e.js";import{p as t,L as i
             .highlight-off {
                 background-color: inherit;
             }
-        `}}customElements.define("pb-keyword",l);
+        `}}customElements.define("pb-keyword",s);
--- pb-extension-bundle.js
+++ pb-extension-bundle.js
@@ -1,6 +1,5 @@
 /*
  * Add your components here to include them in the generated bundles
  */
-import './src/pb-clipboard.js';
 import './src/pb-iframe.js';
 import './src/pb-keyword.js';
